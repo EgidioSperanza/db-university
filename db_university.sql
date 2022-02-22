ad42035c86ae66a0909c57c5abce9f3c993b0c74
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-02-22 15:23:53 +0100

    LIKE used

--- db_university.sql
+++ db_university.sql
@@ -19,6 +19,12 @@ FROM `courses`
 WHERE `period` = 'I semestre'
     AND `year` = 1
 ORDER BY `name` 
+--OPPURE
+SELECT *
+FROM `courses`
+WHERE `period` LIKE "I %"
+    AND `year` = 1
+ORDER BY `name` 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT *
 FROM `exams`
