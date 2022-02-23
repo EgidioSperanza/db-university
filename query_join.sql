44f0d01bccd80ca8c14f9bc922473cad24b9ae26
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-02-23 22:26:57 +0100

    -fix bonus

--- query_join.sql
+++ query_join.sql
@@ -37,8 +37,8 @@ INNER JOIN `course_teacher`
 INNER JOIN `teachers` 
 	ON `course_teacher`.`teacher_id` = `teachers`.`id`
 ORDER BY `degrees`.`name`
--- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)--ERROR 
-SELECT CONCAT(`teachers`.`surname`, " " , `teachers`.`name`) AS "Teachers of Mathematics Department"
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT DISTINCT CONCAT(`teachers`.`surname`, " " , `teachers`.`name`) AS "Teachers of Mathematics Department"
 FROM `teachers`
 INNER JOIN `course_teacher`
 	ON `course_teacher`.`teacher_id`=`teachers`.`id`
@@ -65,5 +65,5 @@ INNER JOIN `exams`
 INNER JOIN `courses`
 	ON `courses`.`id`= `exams`.`course_id`
 WHERE `exam_student`.`vote`>=18
-GROUP BY CONCAT(`students`.`surname`, " ", `students`.`surname`), `courses`.`name`
-ORDER BY CONCAT(`students`.`surname`, " ", `students`.`surname`)
+GROUP BY CONCAT(`students`.`surname`, " ", `students`.`name`), `courses`.`name`
+ORDER BY CONCAT(`students`.`surname`, " ", `students`.`name`)
