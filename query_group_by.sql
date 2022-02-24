b730702d1c302aeac32b6d9696df4e0a2b7fc010
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-02-24 10:22:01 +0100

    Added a HAVING condition for shared offices

--- query_group_by.sql
+++ query_group_by.sql
@@ -1,11 +1,12 @@
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-SELECT YEAR(`enrolment_date`) AS "Anno Accademico", COUNT(id) AS "Studenti Iscritti"
+SELECT YEAR(`enrolment_date`) AS "Anno Accademico", COUNT(`id`) AS "Studenti Iscritti"
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT `office_address` AS " Office Address", COUNT(`id`) AS "Teachers"
 FROM `teachers`
 GROUP BY `office_address`
+    HAVING COUNT(`id`)>1
 -- 2.Bonus Offices Number
 SELECT COUNT(DISTINCT `office_address`) AS "Numero di Uffici"
 FROM `teachers`
@@ -14,6 +15,6 @@ SELECT `exam_id` AS "Esame", AVG(`vote`) AS "Media Voti"
 FROM `exam_student`
 GROUP by `exam_id`
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
-SELECT `department_id` AS "Dipartimenti", COUNT(id) AS "Corsi di Laurea"
+SELECT `department_id` AS "Dipartimenti", COUNT(`id`) AS "Corsi di Laurea"
 FROM `degrees`
 GROUP BY `department_id`
